64d46edf94e32a7be6f7eeef68cca43a16c67103
Adriano Tisera <axenide@gmail.com>
2023-03-28 18:34:16 -0300

    Corrección

--- Clase_1/3.py
+++ Clase_1/3.py
@@ -1,33 +1,41 @@
 import argparse
 import sys
 
+def count(file):
+    lines = 0
+    words = 0
+    chars = 0
+
+    for line in file:
+        lines += 1
+        words += len(line.split())
+        chars += len(line)
+
+    return lines, words, chars
+
+def average_word_length(file):
+    lines, words, chars = count(file)
+    return (chars / words)
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="indicate the file to count the number of words and lines", type=str)
-    parser.add_argument("-a", "--average", help="print the average length of words in the file", action="store_true")
+    parser.add_argument('-f', type=argparse.FileType('r'))
+    parser.add_argument('-p', action='store_true')
     args = parser.parse_args()
 
     try:
-        with open(args.file, "r") as file:
-            lines = file.readlines()
-            words = 0
-            total_word_length = 0
-            for line in lines:
-                words_in_line = line.split()
-                words += len(words_in_line)
-                if args.average:
-                    total_word_length += sum(len(word) for word in words_in_line)
-            print("The file has {} lines and {} words".format(len(lines), words))
-            if args.average:
-                average_word_length = total_word_length / words
-                print("The average length of words in the file is {:.2f}".format(average_word_length))
-    except FileNotFoundError:
-        with open("errors.log", "a") as error_file:
-            error_file.write("The file {} does not exist\n".format(args.file))
-        sys.stderr.write("The file {} does not exist\n".format(args.file))
+        file_content = args.f.readlines()
+        lines, words, chars = count(file_content)
+        print("El archivo tiene ", lines, " líneas y ", words, " palabras.")
+
+        if args.p:
+            print("El promedio de longitud de las palabras es ", str("{:.2f}".format(average_word_length(file_content))) + ".")
+
+    except Exception as e:
+        with open('error.log', 'w') as f:
+            f.write(str(e))
+        print("Error: ", e)
         sys.exit(1)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
