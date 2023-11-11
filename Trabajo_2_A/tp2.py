b40e51cef6457dfc8b5c0b1a2dd5ecad2d827835
AdrianoTisera <adrianotisera@gmail.com>
2023-11-11 18:43:20 -0300

    TP2-A WIP

---
+++ Trabajo_2_A/tp2.py
@@ -0,0 +1,50 @@
+import argparse
+import http.server
+import socket
+from PIL import Image
+from io import BytesIO
+import multiprocessing
+import magic
+
+def grayscale_image(image_data):
+    with Image.open(BytesIO(image_data)) as img:
+        gray = img.convert('L')
+        buf = BytesIO()
+        gray.save(buf, format='JPEG')
+        return buf.getvalue()
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        
+        # Use python-magic to detect the file type
+        mime = magic.Magic()
+        file_type = mime.from_buffer(post_data)
+        
+        if file_type.startswith('image'):
+            pool = multiprocessing.Pool(processes=1)
+            result = pool.apply_async(grayscale_image, [post_data])
+            gray_img_data = result.get()
+            self.send_response(200)
+            self.send_header('Content-type', 'image/jpeg')
+            self.end_headers()
+            self.wfile.write(gray_img_data)
+        else:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Error: El archivo no es una imagen.')
+
+def run(server_class=http.server.HTTPServer, handler_class=CustomHTTPRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="TP2 - procesa imágenes")
+    parser.add_argument('-p', '--port', type=int, default=8000, help='Puerto de escucha')
+    parser.add_argument('-i', '--ip', default=socket.gethostname(), help='Dirección de escucha')
+    args = parser.parse_args()
+    run(port=args.port)
